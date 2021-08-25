02f6c7d1c2c7e7a3d28aa4a8380bae257edcdd8a
lankysquid <swebster08@gmail.com>
2021-08-24 21:22:23 -0700

    Working Queue

--- src/edu/ecrchs/Main.java
+++ src/edu/ecrchs/Main.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
         Stack<String> stack = new Stack<String>();
         Scanner in = new Scanner(System.in);
-        System.out.println("Input names, type 'quit' to end");
+        System.out.println("Input names for a stack, type 'quit' to end");
         String name = in.nextLine();
         while (!name.equalsIgnoreCase("quit")) {
             stack.push(name);
@@ -17,6 +16,18 @@ public class Main {
         }
         System.out.println("\nYour stack order:");
         for (String s : stack) {
+            System.out.println(s);
+        }
+
+        Queue<String> queue = new Queue<String>();
+        System.out.println("Input names for a queue, type 'quit' to end");
+        name = in.nextLine();
+        while (!name.equalsIgnoreCase("quit")) {
+            queue.enqueue(name);
+            name = in.nextLine();
+        }
+        System.out.println("\n Your Queue order:");
+        for(String s : queue) {
             System.out.println(s);
         }
     }
--- src/edu/ecrchs/Queue.java
+++ src/edu/ecrchs/Queue.java
@@ -1,15 +1,69 @@
 package edu.ecrchs;
 
-public class Queue<Item> {
-    private Node<Item> last;
-    private int num;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Queue<Item> implements Iterable<Item> {
+    private Node<Item> first;    //beginning of queue
+    private Node<Item> last;     //end of queue
+    private int num;             //number of elements in the array
+
+    public Queue() {
+        first = null;
+        last = null;
+        num = 0;
+    }
 
     private static class Node<Item> {
         private Item item;
-        private Node<Item> last;
+        private Node<Item> next;
     }
 
-    private void push (Item item) {
-
+    public boolean isEmpty() {
+        return first == null;
     }
+
+    public void enqueue (Item item) {
+        Node<Item> oldlast = last;
+        last = new Node<Item>();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else oldlast.next = last;
+        num++;
+    }
+
+    public Item dequeue() {
+        if (isEmpty()) throw new NoSuchElementException("Queue is empty");
+        Item item = first.item;
+        first = first.next;
+        num--;
+        if (isEmpty()) last = null;
+        return item;
+    }
+
+    /* For iteration, but does not affect the queue */
+
+    public Iterator<Item> iterator() {
+        return new LinkedIterator(first);
+    }
+
+    private class LinkedIterator implements Iterator<Item> {
+        private Node<Item> current;
+
+        public LinkedIterator(Node<Item> first) {
+            current = first;
+        }
+
+        public boolean hasNext()  { return current != null;                     }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+
 }
